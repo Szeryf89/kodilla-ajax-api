70467f82d7117db7c3249be609d47075d08b8d6c
marcni <marcin.radomski89@gmail.com>
2017-09-25 19:12:56 +0200

    changes in js files

--- module_12.7/App.js
+++ module_12.7/App.js
@@ -1,36 +1,9 @@
-/*
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
-	for (i = 0; i < 10; i++) {
-	  str += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return str;
-}
-
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
-
-// DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
-
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
-
-// DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
-*/
 var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
 var myHeaders = {
   'X-Client-Id': 2284,
   'X-Auth-Token': "6f58ecd5e1184c9db8b5e80322271b5f"
 };
+
 $.ajaxSetup({
 	headers: myHeaders
 });
@@ -54,6 +27,7 @@ function setupCards(col, cards) {
 	cards.forEach(function (card) {
         var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
     	col.createCard(card);
-    	
+
   	})
 }
+
