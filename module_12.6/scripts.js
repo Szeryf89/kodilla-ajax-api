acb6deabba9043f98018a45e9383bd3366cfc3f2
marcni <marcin.radomski89@gmail.com>
2017-09-25 18:18:24 +0200

    add module 12.6

--- module_12.6/scripts.js
+++ module_12.6/scripts.js
@@ -1,6 +1,8 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('#countries');
 
+$('#search').click(searchCountries);
+
 function searchCountries() {
     var countryName = $('#country-name').val();
 if(!countryName.length) countryName = 'Poland';
